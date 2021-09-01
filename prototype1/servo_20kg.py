23704cca52734dfe3888924adcbf1d2d0de5c725
L-fu-des22 <71368999+L-fu-des22@users.noreply.github.com>
2021-09-01 13:08:31 +0800

    Update servo_20kg.py

--- prototype1/servo_20kg.py
+++ prototype1/servo_20kg.py
@@ -4,9 +4,9 @@
 import pigpio
 import time
 pi=pigpio.pi()
-pi.write(6, 0) # BCM 将4号引脚设置为低电平
+pi.write(6, 0) # BCM 将6号引脚设置为低电平
 pi.read(6)
-pi.set_PWM_frequency(6, 50)#设定14号引脚产生的pwm波形的频率为50Hz
+pi.set_PWM_frequency(6, 50)#设定6号引脚产生的pwm波形的频率为50Hz
 pi.set_PWM_range(6, 2000)
 #指定要把14号引脚上的一个pwm周期分成多少份，这里是分成2000份，这个数据的范围是25-40000        
 #The range of pwm is (50,250),One serving corresponds to 0.9°
@@ -14,7 +14,7 @@ pi.set_PWM_range(6, 2000)
 #range:95(CCW)~140(vertical)~185(CW) 
 pi.set_PWM_dutycycle(6,140)
 time.sleep(0.5)
-#指定pwm波形的占空比，这里的占空比为150/2000,2000是上一个函数设定的
+#指定pwm波形的占空比，这里的占空比为140/2000,2000是上一个函数设定的
 
 
 
